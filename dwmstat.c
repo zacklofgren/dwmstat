79cf456ca1617f88f848238899d510ba6a058b23
Klemens Nanni <kl3@posteo.org>
2016-12-18 23:20:49 +0100

    Add time, fix variable names

--- dwmstat.c
+++ dwmstat.c
@@ -1,14 +1,33 @@
 #include <err.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
 #include <X11/Xlib.h>
 
 static Display *dpy;
 
-void
-setstatus(const char *str)
+static const char*
+_time(void)
 {
-	XStoreName(dpy, DefaultRootWindow(dpy), str);
+	time_t t;
+	struct tm *tm;
+	static char s[26];
+
+	if ((t = time(NULL)) == (time_t)-1)
+		errx(1, "could not get current system time");
+	if (!(tm = localtime(&t)))
+		errx(1, "could not convert system to local time");
+	if (!strftime(s, sizeof(s), "%a %d.%m.%y %H:%M", tm))
+		errx(1, "could not convert local time to string");
+
+	return s;
+}
+
+static void
+setstatus(const char *s)
+{
+	XStoreName(dpy, DefaultRootWindow(dpy), s);
 	XSync(dpy, False);
 }
 
@@ -17,6 +36,8 @@ main(void)
 {
 	if (!(dpy = XOpenDisplay(NULL)))
 		errx(1, "cannot open display");
+
+	setstatus(_time());
 
 	XCloseDisplay(dpy);
 
