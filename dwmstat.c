463b84b37910efa95ba78e23b20d389152fb9512
Klemens Nanni <kl3@posteo.org>
2016-12-19 11:06:14 +0100

    Add system volume

--- dwmstat.c
+++ dwmstat.c
@@ -1,9 +1,15 @@
 #include <err.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/ioctl.h>
+#include <sys/audioio.h>
+#include <string.h>
 #include <time.h>
 #include <unistd.h>
 #include <X11/Xlib.h>
+
+#define MAX(a,b) (((a) > (b)) ? (a) : (b))
 
 static Display *dpy;
 
@@ -24,6 +30,49 @@ _time(void)
 	return s;
 }
 
+static const unsigned int
+_volume(void)
+{
+	static int cls;
+	struct mixer_devinfo mdi;
+	struct mixer_ctrl mc;
+	int m;
+	int v;
+
+	if ((m = open("/dev/mixer", O_RDONLY)) == -1)
+		errx(1, "could not open mixer device");
+
+	cls = -1;
+	v = -1;
+
+	for (mdi.index = 0; cls == -1; ++mdi.index) {
+		if (ioctl(m, AUDIO_MIXER_DEVINFO, &mdi) == -1)
+			errx(1, "could not get audio mixer information");
+		if (mdi.type == AUDIO_MIXER_CLASS &&
+		    !strcmp(mdi.label.name, AudioCoutputs))
+			cls = mdi.index;
+	}
+	for (mdi.index = 0; v == -1; ++mdi.index) {
+		if (ioctl(m, AUDIO_MIXER_DEVINFO, &mdi) == -1)
+			errx(1, "could not get audio mixer information");
+		if (mdi.type == AUDIO_MIXER_VALUE &&
+		    mdi.prev == AUDIO_MIXER_LAST &&
+		    mdi.mixer_class == cls &&
+		    !strcmp(mdi.label.name, AudioNmaster)) {
+			mc.dev = mdi.index;
+			if (ioctl(m, AUDIO_MIXER_READ, &mc) == -1)
+				errx(1, "could not read audio mixer");
+			v = mc.un.value.num_channels == 1 ?
+			    mc.un.value.level[AUDIO_MIXER_LEVEL_MONO] :
+			    MAX(mc.un.value.level[AUDIO_MIXER_LEVEL_LEFT],
+			        mc.un.value.level[AUDIO_MIXER_LEVEL_RIGHT]);
+		}
+	}
+	if (v == -1)
+		errx(1, "could not get system volume");
+	return (v * 100) / 255;
+}
+
 static void
 setstatus(const char *s)
 {
@@ -37,7 +86,9 @@ main(void)
 	if (!(dpy = XOpenDisplay(NULL)))
 		errx(1, "cannot open display");
 
-	setstatus(_time());
+	printf("%3d%%  %s",
+	       _volume(),
+	       _time());
 
 	XCloseDisplay(dpy);
 
