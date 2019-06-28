f5d258927e0c6e0281aca6aec15a79cfab3e72a6
Zack Lofgren <zack@zlofgren.com>
2019-06-28 11:30:14 -0600

    changed config.def.h to config.h, change network device to em0, and changed date and OUTFMT formatting

--- config.h
+++ config.h
@@ -1,10 +1,10 @@
 /* interface to show IP address for */
-#define INTERFACE "trunk0"
+#define INTERFACE "em0"
 /* seconds to wait between polling */
-#define INTERVAL  60
+#define INTERVAL  30
 /* maximum number of characters in status string */
 #define MAX_LEN   128
 /* output format used by printf(3) for status string */
-#define OUTFMT    "%s  %3d%%  %3d°C  %3d%%  %s"
+#define OUTFMT    "%s | %3d%% | %3d°C | %3d%% | %s"
 /* time format used by strftime(3) within status string */
-#define TIMEFMT   "%a %d.%m.%y %H:%M"
+#define TIMEFMT   "%a %Y.%m.%d %H:%M"
